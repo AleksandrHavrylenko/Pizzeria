6621847ac93af941c54dec29e22d78299bef9e38
Vlad Mochonir <vlad17021995m@gmail.com>
2017-03-31 21:38:17 +0300

    update test class for dishSerice

--- Pizzeria/src/test/java/com/xzteam/pizzeria/services/DishServiceTest.java
+++ Pizzeria/src/test/java/com/xzteam/pizzeria/services/DishServiceTest.java
@@ -1,5 +1,6 @@
 package com.xzteam.pizzeria.services;
 
+import com.xzteam.pizzeria.api.dish.DishApi;
 import com.xzteam.pizzeria.domain.Dish;
 import com.xzteam.pizzeria.domain.enums.DishType;
 import org.junit.Assert;
@@ -7,11 +8,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 /**
  * Created by qwerty on 31.03.2017.
@@ -24,10 +28,16 @@ public class DishServiceTest {
     @Autowired
     private DishService dishService;
 
+    @Test
+    @Sql("before_test.sql")
+    @Sql(scripts = "before_test.sql", executionPhase = BEFORE_TEST_METHOD)
     public void before(){
 
     }
 
+    @Test
+    @Sql("after_test.sql")
+    @Sql(scripts = "after_test.sql", executionPhase = AFTER_TEST_METHOD)
     public void after(){
 
     }
@@ -35,12 +45,13 @@ public class DishServiceTest {
     @Test
     public void getAll() throws Exception {
         List<Dish> dishes = dishService.getAll();
-        //Assert.assertNull(dishes);
+        Assert.assertNotNull(dishes);
     }
 
     @Test
     public void getDishById() throws Exception {
         Dish dish = dishService.getDishById(dishService.getAll().get(0).getId());
+        Assert.assertNotNull(dish);
     }
 
     @Test
@@ -52,23 +63,64 @@ public class DishServiceTest {
         dish.setPrice(560.0f);
         dish.setType(DishType.DESSERT);
         dishService.addDish(dish);
+
+        Dish d = dishService.getAll().get(0);
+        Assert.assertNotNull(d);
+        assertEquals(d.getName(), dish.getName());
+        assertEquals(d.getDescription(), dish.getDescription());
+        assertEquals(d.getPrice(), dish.getPrice());
+        assertEquals(d.getWeight(), dish.getWeight());
+        assertEquals(d.getType(), dish.getType());
+        dishService.deleteDish(d.getId());
     }
 
     @Test
     public void updateDish() throws Exception {
-        Dish dish = dishService.getAll().get(0);
-        dishService.updateDish(dish);
+        Dish d = dishService.getAll().get(0);
+        d.setName("TestDish");
+        d.setDescription("TestDescription");
+        d.setPrice(100.0f);
+        d.setWeight(200);
+        d.setType(DishType.SECOND_DISH);
+        dishService.updateDish(d);
+
+        Dish de = dishService.getAll().get(0);
+        Assert.assertNotNull(de);
+        assertEquals(d.getName(), de.getName());
+        assertEquals(d.getDescription(), de.getDescription());
+        assertEquals(d.getPrice(), de.getPrice());
+        assertEquals(d.getWeight(), de.getWeight());
+        assertEquals(d.getType(), de.getType());
     }
 
     @Test
     public void deleteDish() throws Exception {
-        dishService.deleteDish(dishService.getAll().get(0).getId());
+        Dish dish = new Dish();
+        dish.setDescription("description1");
+        dish.setWeight(40);
+        dish.setName("name1");
+        dish.setPrice(560.0f);
+        dish.setType(DishType.DESSERT);
+        dishService.addDish(dish);
+
+        Dish d = dishService.getAll().get(0);
+        Assert.assertNotNull(d);
+        assertEquals(d.getName(), dish.getName());
+        assertEquals(d.getDescription(), dish.getDescription());
+        assertEquals(d.getPrice(), dish.getPrice());
+        assertEquals(d.getWeight(), dish.getWeight());
+        assertEquals(d.getType(), dish.getType());
+        Long id = d.getId();
+        dishService.deleteDish(d.getId());
+        dish = dishService.getDishById(id);
+        Assert.assertNull(dish);
     }
 
     @Test
     public void exists() throws Exception {
         Dish dish = dishService.getAll().get(0);
-        Assert.assertNotNull(dish);
+        boolean b = dishService.exists(dish.getId());
+        Assert.assertTrue(b);
     }
 
 }
